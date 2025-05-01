871fbe72306388e135e713e8004795eed5514479
k.dobos <krisztian.dobos@t-systems.com>
2025-05-01 10:34:06 +0200

    added ssh key management to terraform

--- vm_config/node-0/startup.sh
+++ vm_config/node-0/startup.sh
@@ -4,6 +4,7 @@ sudo sed -i \
   's/^#*PermitRootLogin.*/PermitRootLogin yes/' \
   /etc/ssh/sshd_config
 
+mkdir /root/.ssh
 public_key=$(curl -s "http://metadata.google.internal/computeMetadata/v1/instance/attributes/ssh-keys" -H "Metadata-Flavor: Google")
 sudo echo "${public_key}" > /root/.ssh/authorized_keys
 sudo chmod 600 /root/.ssh/authorized_keys
