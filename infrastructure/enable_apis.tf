971303cb21c998f33b3ce4d558e97c050b3e6036
k.dobos <krisztian.dobos@t-systems.com>
2025-05-02 10:35:22 +0200

    fixed api disable on destroy

--- infrastructure/enable_apis.tf
+++ infrastructure/enable_apis.tf
@@ -1,27 +1,34 @@
 resource "google_project_service" "secret_manager" {
   service = "secretmanager.googleapis.com"
+  disable_on_destroy = false
 }
 
 resource "google_project_service" "compute" {
   service = "compute.googleapis.com"
+  disable_on_destroy = false
 }
 
 resource "google_project_service" "iam" {
   service = "iam.googleapis.com"
+  disable_on_destroy = false
 }
 
 resource "google_project_service" "cloudresourcemanager" {
   service = "cloudresourcemanager.googleapis.com"
+  disable_on_destroy = false
 }
 
 resource "google_project_service" "storage" {
   service = "storage.googleapis.com"
+  disable_on_destroy = false
 }
 
 resource "google_project_service" "logging" {
   service = "logging.googleapis.com"
+  disable_on_destroy = false
 }
 
 resource "google_project_service" "monitoring" {
   service = "monitoring.googleapis.com"
+  disable_on_destroy = false
 }
